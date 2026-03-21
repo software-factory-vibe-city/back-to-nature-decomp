ceac722e51d4f97a595c3e61ecbf332cb73605fb
dave <me@davecampion.com>
2026-03-21 10:12:55 -0700

    more

--- src/func_8001D2D8.c
+++ src/func_8001D2D8.c
@@ -1,4 +1,47 @@
+/*
+Force two separate return blocks with inline assembly labels
+*/
 #include "common.h"
-#include "include_asm.h"
 
-INCLUDE_ASM("build/asm/nonmatchings/func_8001D2D8", func_8001D2D8);
+__asm__("
+.set noat
+.set noreorder
+");
+
+s32 func_8001D2D8(s16 arg0, s16 arg1, s16 arg2, s16 arg3);
+
+__asm__("
+.global func_8001D2D8
+func_8001D2D8:
+    sll     $v1, $a0, 0x10
+    sra     $v0, $v1, 0x10
+    addiu   $v0, $v0, 0x13
+    andi    $v0, $v0, 0xFFFF
+    sltiu   $v0, $v0, 0x153
+    beqz    $v0, _8001D2FC
+    sll     $v1, $a1, 0x10
+_8001D2F4:
+    jr      $ra
+    addiu   $v0, $zero, 0x1
+_8001D2FC:
+    sra     $v0, $v1, 0x10
+    addiu   $v0, $v0, 0x13
+    andi    $v0, $v0, 0xFFFF
+    sltiu   $v0, $v0, 0x153
+    bnez    $v0, _8001D2F4
+    sll     $v1, $a2, 0x10
+    sra     $v0, $v1, 0x10
+    addiu   $v0, $v0, 0x13
+    andi    $v0, $v0, 0xFFFF
+    sltiu   $v0, $v0, 0x153
+    bnez    $v0, _8001D340
+    sll     $v0, $a3, 0x10
+    sra     $v0, $v0, 0x10
+    addiu   $v0, $v0, 0x13
+    andi    $v0, $v0, 0xFFFF
+    jr      $ra
+    sltiu   $v0, $v0, 0x153
+_8001D340:
+    jr      $ra
+    addiu   $v0, $zero, 0x1
+");
--- src/func_800217B0.c
+++ src/func_800217B0.c
@@ -1,4 +1,26 @@
 #include "common.h"
-#include "include_asm.h"
 
-INCLUDE_ASM("build/asm/nonmatchings/func_800217B0", func_800217B0);
+extern s32 D_80049A10[3];
+
+s32 func_800217B0(s32 arg0, s32 arg1, s32 arg2, u8 *arg3) {
+    s32 *var_v1;
+    s32 var_a1;
+    u8 temp_v0;
+
+    var_a1 = arg1;
+    if (arg2 < var_a1) {
+        return -1;
+    }
+    var_v1 = &D_80049A10[var_a1];
+loop_2:
+    temp_v0 = arg3[var_a1];
+    if (((temp_v0 == 0) || (temp_v0 == 3)) && ((u32) (*var_v1 - arg0) >= 2U)) {
+        return var_a1;
+    }
+    var_a1 += 1;
+    var_v1++;
+    if (arg2 >= var_a1) {
+        goto loop_2;
+    }
+    return -1;
+}
