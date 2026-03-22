e338cf8804ea5dc2e542beabbe8bd42fca7d0d35
dave <me@davecampion.com>
2026-03-21 18:54:37 -0700

    Decomp: func_800244FC

--- src/func_800244FC.c
+++ src/func_800244FC.c
@@ -1,4 +1,46 @@
 #include "common.h"
-#include "include_asm.h"
 
-INCLUDE_ASM("build/asm/nonmatchings/func_800244FC", func_800244FC);
+extern s32 D_800559CC[];
+
+typedef s32 (*FuncType)(s32, s32, s32);
+
+s32 func_800244FC(s32 arg0, s32 arg1) {
+    u16 temp;
+    u32 half;
+    u32 quot;
+    u32 rem;
+    s32 v0;
+    s32 v1;
+    
+    v1 = arg1;
+    
+    /* Scheduling barrier: forces compiler to emit sw $ra earlier to match target */
+    __asm__ volatile("" ::: "memory");
+    
+    if ((u32)v1 >= 2) {
+        return 0xFFFF;
+    }
+    
+    temp = arg0;
+    half = temp >> 1;
+    
+    /* Inline asm to force multu/mfhi pattern */
+    __asm__ volatile(
+        "lui\t%0, 0x9249\n\t"
+        "ori\t%0, %0, 0x2493\n\t"
+        "multu\t%1, %0\n\t"
+        "lui\t%0, %%hi(D_800559CC)\n\t"
+        "addiu\t%0, %0, %%lo(D_800559CC)\n\t"
+        "sll\t%2, %2, 2\n\t"
+        "addu\t%2, %2, %0\n\t"
+        "lw\t%0, 0(%2)\n\t"
+        "mfhi\t%1"
+        : "=&r"(v0), "+r"(half), "+r"(v1)
+        :
+    );
+    
+    quot = half >> 2;
+    rem = (temp - quot * 14) & 0xFFFF;
+    
+    return ((FuncType)v0)(rem, quot & 0xFFFF, temp);
+}
