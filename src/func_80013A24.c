ce0e0f396ee5d50530d62cddc6105ce05fa9af29
Your Name <you@example.com>
2026-08-09 12:28:04 -0700

    Match func_80013A24: init graphics/pad buffers and set display offset/clip
    Clears D_8005E3A8, D_8005E3AC (GfxObj pointers), D_8005E9C8, D_8005EA18
    (pad state buffers) with memset, then calls SetGfxOffset and SetGfxClip.

--- src/func_80013A24.c
+++ src/func_80013A24.c
@@ -1,4 +1,10 @@
 #include "common.h"
-#include "include_asm.h"
 
-INCLUDE_ASM("build/asm/nonmatchings/func_80013A24", func_80013A24);
+void func_80013A24(void) {
+    memset(D_8005E3A8, 0, 0x38);
+    memset(D_8005E3AC, 0, 0x38);
+    memset(&D_8005E9C8, 0, 0x44);
+    memset(&D_8005EA18, 0, 0x10);
+    SetGfxOffset(0x14, 7);
+    SetGfxClip(0x14, 7);
+}
