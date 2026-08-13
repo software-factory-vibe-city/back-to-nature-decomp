0f3c922738941d6c521e450958d2b550c3c3c2de
Your Name <you@example.com>
2026-08-12 23:07:48 -0700

    match func_80014FAC
    Byte-exact and finalized by /auto_decompilation_loop on qwen3.6-27b (local).

--- src/func_80014FAC.c
+++ src/func_80014FAC.c
@@ -1,4 +1,5 @@
 #include "common.h"
-#include "include_asm.h"
 
-INCLUDE_ASM("build/asm/nonmatchings/func_80014FAC", func_80014FAC);
+void func_80014FAC(s32 *arg0, s16 arg1, s16 arg2, s16 arg3, s16 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8, s16 arg9) {
+    func_800153BC(func_80011F5C(0x24), arg0, arg1, arg2, (s16) (s32) arg3, (s16) (s32) arg4, arg5, arg6, arg7, arg8, (s16) (s32) arg9);
+}
