47e32dd01f607a587c9fefb571ac874589d33a92
Your Name <you@example.com>
2026-08-20 06:42:51 -0700

    match func_8001AFE0
    Byte-exact and finalized by /auto_decompilation_loop on deepseek-v4-flash.

--- src/func_8001AFE0.c
+++ src/func_8001AFE0.c
@@ -1,4 +1,8 @@
 #include "common.h"
-#include "include_asm.h"
 
-INCLUDE_ASM("build/asm/nonmatchings/func_8001AFE0", func_8001AFE0);
+void func_8001B074();
+
+void func_8001AFE0(void) {
+    memset(&D_8005F2E8, 0, 0x1300);
+    func_8001B074(0, 0, 0, 0, 0, 0);
+}
